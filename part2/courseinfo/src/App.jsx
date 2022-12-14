d419a5bb5f0cddd0b4f5832997dd8d5aa55ba515
ikbakkk <iqbalfirdaus05@gmail.com>
2022-12-14 15:38:50 +0800

    2.1 - 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,29 +1,35 @@
 import Course from "./compoonents/Course";
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
+  const courses = [
+    {
+      id: 1,
+      name: "Half Stack application development",
+      parts: [
+        { id: 1, name: "Fundamentals of React", exercises: 10 },
+        { id: 2, name: "Using props to pass data", exercises: 7 },
+        { id: 3, name: "State of a component", exercises: 14 },
+        { id: 4, name: "Redux", exercises: 11 },
+      ],
+    },
+    {
+      id: 2,
+      name: "Node.js",
+      parts: [
+        { id: 1, name: "Routing", exercises: 3 },
+        { id: 2, name: "Middlewares", exercises: 7 },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  return (
+    <>
+      <h1>Web Development curriculum</h1>
+      {courses.map((item) => (
+        <Course key={item.id} course={course} />
+      ))}
+    </>
+  );
 };
 
 export default App;
