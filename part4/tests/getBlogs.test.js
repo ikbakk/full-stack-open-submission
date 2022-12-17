ca02e9e08ede39b77915b92bbd4df485d492f199
ikbakkk <iqbalfirdaus05@gmail.com>
2022-12-17 23:36:52 +0800

    4.8

---
+++ part4/tests/getBlogs.test.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const BlogObjects = helper.initialBlogs.map((blog) => new Blog(blog))
+  const promiseArray = BlogObjects.map((blog) => blog.save())
+  await Promise.all(promiseArray)
+})
+
+describe('HTTP GET /api/blogs ', () => {
+  test('http application returns the correct amount of blog', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body.length).toBe(helper.initialBlogs.length)
+  })
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
