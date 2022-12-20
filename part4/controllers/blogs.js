0c03c7ad2ea2b6e58c02f615d686ea380fd53340
ikbakkk <iqbalfirdaus05@gmail.com>
2022-12-20 13:18:40 +0800

    4.13 - 4.14

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -7,22 +7,38 @@ blogsRouter.get('/', (request, response) => {
   })
 })
 
-blogsRouter.post('/', (request, response) => {
-  if (request.body.title === undefined) {
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    id: request.params.id
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true
+  })
+  response.json(updatedBlog.toJSON())
+})
+
+blogsRouter.post('/', async (request, response) => {
+  if (request.body.title == undefined) {
     response.status(400).json('title not found')
-  } else if (request.body.url === undefined) {
+  } else if (request.body.url == undefined) {
     response.status(400).json('url not found')
   } else {
     const blog = new Blog(request.body)
-    blog.save().then((result) => {
-      response.status(201).json(result)
-    })
+    const savedBlog = await blog.save()
+    response.json(savedBlog.toJSON())
   }
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
 })
 
 module.exports = blogsRouter
