1c8fee28ea32253d197247f53e6119ab6022064d
ikbakk <iqbalfirdaus05@gmail.com>
2023-05-24 20:13:25 -0700

    5.9

--- part5/src/App.js
+++ part5/src/App.js
@@ -92,9 +92,11 @@ const App = () => {
             <BlogForm createBlog={createBlog} />
           </Toggle>
           <div className='blog-list'>
-            {blogs?.map(blog => (
-              <Blog key={blog.id} blog={blog} likeUpdate={likeUpdate} />
-            ))}
+            {blogs
+              ?.sort((a, b) => b.likes - a.likes)
+              .map(blog => (
+                <Blog key={blog.id} blog={blog} likeUpdate={likeUpdate} />
+              ))}
           </div>
         </div>
       )}
