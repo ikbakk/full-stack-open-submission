f90439d28699e8e4b83f65e91a9ae80339faac5d
ikbakk <iqbalfirdaus05@gmail.com>
2023-05-25 02:54:04 -0700

    5.23

--- part5/cypress/e2e/blog_app.cy.js
+++ part5/cypress/e2e/blog_app.cy.js
@@ -80,6 +80,50 @@ describe('Blog app', () => {
         .click();
       cy.wait(1000);
       cy.get('.delete-btn').should('exist').click();
+      cy.get('html').should('not.contain', 'Cypress make this');
+    });
+  });
+
+  describe('When lot of blogs', function () {
+    beforeEach(function () {
+      cy.login(loginCredential);
+      cy.createBlog({
+        title: 'Cypress make this',
+        author: 'Cypress',
+        url: 'www.cypress.io'
+      });
+      cy.createBlog({
+        title: 'Cypress make this2',
+        author: 'Cypress',
+        url: 'www.cypress.io'
+      });
+      cy.createBlog({
+        title: 'Cypress make this3',
+        author: 'Cypress',
+        url: 'www.cypress.io'
+      });
+    });
+
+    it('blog with most like is on top', async function () {
+      cy.contains('Cypress make this3')
+        .parent()
+        .find('.visibility-button')
+        .click();
+      cy.get('.like-btn').click().wait(300).click().wait(500);
+      cy.contains('Cypress make this3')
+        .parent()
+        .find('.visibility-button')
+        .click();
+
+      cy.contains('Cypress make this2')
+        .parent()
+        .find('.visibility-button')
+        .click();
+      cy.get('.like-btn').click().wait(300).click().wait(500).click().wait(500);
+
+      cy.get('.blog-btn').eq(0).should('contain', 'Cypress make this2');
+      cy.get('.blog-btn').eq(1).should('contain', 'Cypress make this3');
+      cy.get('.blog-btn').eq(2).should('contain', 'Cypress make this');
     });
   });
 });
